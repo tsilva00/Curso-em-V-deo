6b8c5afdc0f0a6aa36f5961910de4a989d6c5e99
tsilva00 <73835477+tsilva00@users.noreply.github.com>
2021-05-06 16:55:13 -0300

    :memo: update
    update de comentários

--- pythonexercicios/ex006.py
+++ pythonexercicios/ex006.py
@@ -1,3 +1,5 @@
+#código que recebe um número e devolve o seu dobro, triplo e a raiz quadrada
+
 n = int(input('Escolha um número:'))
 
 print('O dobro do número {} é {}!\nO triplo é {}!\nA raiz quadrada de {} é {}!'.format(n, (n*2), (n*3), n, (n**(1/2))))
