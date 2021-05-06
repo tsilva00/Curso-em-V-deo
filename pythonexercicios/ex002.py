7a0168bea8ef6d5716c1403be4bff77152ac353f
tsilva00 <73835477+tsilva00@users.noreply.github.com>
2021-05-06 16:35:28 -0300

    :memo: Update
    Update de comentário

--- pythonexercicios/ex002.py
+++ pythonexercicios/ex002.py
@@ -1,3 +1,4 @@
-name = input('Digite o seu nome:')
+#código que lê um nome provido pelo usuário e depois utiliza esse nome para preencher uma frase padrão(pré-estabelecida)
 
+name = input('Digite o seu nome:')
 print('É um prazer te conhecer, {}!'.format(name))
