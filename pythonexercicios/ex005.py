3e43f39dafd8bf2c2d7d7237c0faf340b658b58d
tsilva00 <73835477+tsilva00@users.noreply.github.com>
2021-05-06 16:39:51 -0300

    :memo: update
    update de comentário

--- pythonexercicios/ex005.py
+++ pythonexercicios/ex005.py
@@ -1,3 +1,5 @@
+#código que recebe um número provido pelo usuário e retorna o seu antecessor e seu sucessor
+
 n = int(input('Escolha um número:'))
 
 print('O antecessor de {} é {} e o sucessor é {}!'.format(n, (n-1), (n+1)))
